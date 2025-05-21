80d1de28f3863ae6aa6d373c86a84d4d53b401cb
Nikita <savvin.nikit@yandex.ru>
2025-05-21 07:31:40 +0300

    fix: chrome

--- src/superjob/check_res.py
+++ src/superjob/check_res.py
@@ -5,6 +5,6 @@ cwd = os.getcwd()
 
 path_to_save_result = os.path.join(cwd, "src", "superjob", "results")
 
-links_df = pd.read_csv(f'{path_to_save_result}/level_0_links.csv')
+links_df = pd.read_csv(f'{path_to_save_result}/vacancy_links.csv')
 
 print(f" Link counts = {len(links_df)}")
