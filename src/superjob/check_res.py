d121a47291fb700d94cdc2002a441b16fc6c84be
Nikita <savvin.nikit@yandex.ru>
2025-05-21 07:29:23 +0300

    fix: chrome

---
+++ src/superjob/check_res.py
@@ -0,0 +1,10 @@
+import os
+import pandas as pd
+
+cwd = os.getcwd()
+
+path_to_save_result = os.path.join(cwd, "src", "superjob", "results")
+
+links_df = pd.read_csv(f'{path_to_save_result}/level_0_links.csv')
+
+print(f" Link counts = {len(links_df)}")
