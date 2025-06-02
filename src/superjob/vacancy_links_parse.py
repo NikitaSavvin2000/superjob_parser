5da4c38c00715c0b156d25da0c531f7e1c075016
Nikita <savvin.nikit@yandex.ru>
2025-06-02 18:21:58 +0300

    new vers

--- src/superjob/vacancy_links_parse.py
+++ src/superjob/vacancy_links_parse.py
@@ -109,7 +109,6 @@ def create_driver(proxy=None):
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
-    # Remove --user-data-dir to avoid creating a temporary profile
     prefs = {
         "profile.managed_default_content_settings.images": 2,
         "profile.managed_default_content_settings.fonts": 2
